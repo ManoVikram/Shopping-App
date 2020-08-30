1a8b3f94013f37bbf46406f2e52454a2b043e6da
Mano Vikram <manovik18@gmail.com>
2020-08-31 01:32:55 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       ),
       home: ShoppingApp(),
       routes: {
-        ProductDetails.routeName: 
+        ProductDetails.routeName: (contxt) => ProductDetails(),
       },
     );
   }
